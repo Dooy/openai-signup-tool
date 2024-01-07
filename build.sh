f1af170af6cabd1e6ea6b1a8a677f96d94463bdb
Dooy <dooy520@qq.com>
2024-01-07 09:43:44 +0800

    see fail

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # docker build -t chatgpt-web-midjourney-proxy . && docker tag chatgpt-web-midjourney-proxy ydlhero/chatgpt-web-midjourney-proxy:v2.11.2 && docker push  ydlhero/chatgpt-web-midjourney-proxy:v2.11.2
+git pull
 docker build -t openai-signup .
 sh deploy.sh
 docker logs -f openai-signup-tool
